ac106bdc8ca7512dd78c964fd512781ca58b49ab
mithun21-hub <vvce23ise0079@vvce.ac.in>
2026-03-10 16:44:43 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,19 @@
+pipeline {
+    agent any
 
+    stages {
+        stage('Clone') {
+            steps {
+                git url: 'https://github.com/mithun21-hub/jenkins-simple-demo.git',
+                    branch: 'main'
+            }
+        }
+
+        stage('Run Script') {
+            steps {
+                sh 'chmod +x script.sh'
+                sh './script.sh'
+            }
+        }
+    }
+}
